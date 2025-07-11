d267dc2bba636c3a45c76260ac84e1e0e2cd68e3
Zaraksh Rahman <zarakshR@pm.me>
2025-07-11 16:06:49 +0100

    split up values and env into two modules

--- lib/eval.ml
+++ lib/eval.ml
@@ -2,25 +2,30 @@
    let rec eval (env : env) (t : Ast.t) (k : value -> 'a) : 'a = *)
 open Shared
 
-module Value = struct
+module rec Value : sig
   type t =
     | Unit of unit
     | Bool of bool
     | Int of int
     | Pair of t * t
     | Cell of t ref
-    | Thunk of env * Ast.t * t option ref
-    | Closure of env * symbol * Ast.t
+    | Thunk of Env.t * Ast.t * t option ref
+    | Closure of Env.t * symbol * Ast.t
     | Builtin of (t -> t)
   [@@deriving show]
 
-  and env = binding SymTab.t [@@deriving show]
-
-  and binding =
-    (* just a value *)
-    | Plain of t
-    (* a cell that will be filled in with a value later, for recursion *)
-    | Cell of t option ref
+  val equal : t -> t -> bool
+end = struct
+  type t =
+    | Unit of unit
+    | Bool of bool
+    | Int of int
+    | Pair of t * t
+    | Cell of t ref
+    | Thunk of Env.t * Ast.t * t option ref
+    | Closure of Env.t * symbol * Ast.t
+    | Builtin of (t -> t)
+  [@@deriving show]
 
   let rec equal (x : t) (y : t) : bool =
     match (x, y) with
@@ -35,62 +40,98 @@ module Value = struct
         raise (Invalid_argument "equality of builtin")
     (* TODO: types? *)
     | _, _ -> false
+end
 
-  (* trim env w.r.t to `t` *)
-  let trim env t = SymTab.filter (fun x _ -> SymSet.mem x (Ast.free t)) env
+and Env : sig
+  type t [@@deriving show]
+
+  val empty : t
+  val lookup : t -> symbol -> Value.t
+  val extend : t -> symbol -> Value.t -> t
+
+  (* bind symbol to a knot, to be `tie`d later *)
+  val knot : t -> symbol -> t
+
+  (* tie a previously knotted symbol with a value *)
+  val tie : t -> symbol -> Value.t -> unit
+
+  (* trim env w.r.t to `expr` *)
+  val trim : t -> Ast.t -> t
+end = struct
+  type binding =
+    (* just a value *)
+    | Plain of Value.t
+    (* a cell that will be filled in with a value later, for self-reference
+       (recursion, etc.) *)
+    | Knot of Value.t option ref
+  [@@deriving show]
+
+  type t = binding SymTab.t [@@deriving show]
+
+  let empty = SymTab.empty
+
+  let lookup env name =
+    try
+      match SymTab.find name env with
+      | Knot cell -> Option.get !cell
+      | Plain value -> value
+    with Invalid_argument _ | Not_found -> failwith ("lookup: " ^ name)
+
+  let extend env name value = SymTab.add name (Plain value) env
+
+  let knot env name =
+    let cell : Value.t option ref = ref None in
+    let env = SymTab.add name (Knot cell) env in
+    env
+
+  let tie env name value =
+    let[@warning "-8"] (Knot cell) = SymTab.find name env in
+    assert (!cell = None);
+    let _ = cell := Some value in
+    ()
+
+  let trim env expr =
+    SymTab.filter (fun x _ -> SymSet.mem x (Ast.free expr)) env
 end
 
 open Ast
 open Value
 
-let rec eval (env : env) (expr : Ast.t) : Value.t =
+let rec eval (env : Env.t) (expr : Ast.t) : Value.t =
   let module ST = SymTab in
   match expr with
   | Laz e ->
-      let env = trim env e in
+      let env = Env.trim env e in
       Thunk (env, e, ref None)
   | Let (name, e, body) ->
       let res = eval env e in
-      let env = ST.add name (Plain res) env in
+      let env = Env.extend env name res in
       eval env body
   | Fix (binds, body) ->
-      let names, exprs = Seq.unzip (List.to_seq binds) in
-      let names, exprs = (Seq.memoize names, Seq.memoize exprs) in
-      let env =
-        let extend env name = ST.add name (Cell (ref None)) env in
-        Seq.fold_left extend env names
-      in
-      let results = Seq.map (eval env) exprs in
-      let tie (name, result) =
-        let[@warning "-8"] (Cell cell) = ST.find name env in
-        cell := Some result
-      in
-      Seq.iter tie (Seq.zip names results);
+      let names, exprs = List.split binds in
+      let env = List.fold_left Env.knot env names in
+      let results = List.map (eval env) exprs in
+      let _ = List.map2 (Env.tie env) names results in
       eval env body
   | Cnd (test, then_, else_) ->
       let[@warning "-8"] (Bool e) = eval env test in
       if e then eval env then_ else eval env else_
   | Lam (args, body) ->
-      let env = trim env body in
+      let env = Env.trim env body in
       Closure (env, args, body)
   | App (f, arg) -> (
-      let arg = eval env arg in
+      let actual = eval env arg in
       match[@warning "-8"] eval env f with
       | Closure (env, param, body) ->
-          let env = ST.add param (Plain arg) env in
+          let env = Env.extend env param actual in
           eval env body
-      | Builtin builtin -> builtin arg)
-  | Var v -> (
-      try
-        match ST.find v env with
-        | Cell cell -> Option.get !cell
-        | Plain value -> value
-      with Invalid_argument _ | Not_found -> failwith ("not found: " ^ v))
+      | Builtin builtin -> builtin actual)
+  | Var v -> Env.lookup env v
   | LitInt n -> Int n
   | LitBool b -> Bool b
   | LitUnit () -> Unit ()
 
-let[@warning "-8"] stdlib : env =
+let[@warning "-8"] stdlib : Env.t =
   let add = Builtin (fun (Int x) -> Builtin (fun (Int y) -> Int (x + y))) in
   let sub = Builtin (fun (Int x) -> Builtin (fun (Int y) -> Int (x - y))) in
   let mul = Builtin (fun (Int x) -> Builtin (fun (Int y) -> Int (x * y))) in
@@ -121,26 +162,26 @@ let[@warning "-8"] stdlib : env =
             value
         | Some value -> value)
   in
-  SymTab.of_list
-  @@ List.map
-       (fun (sym, impl) -> (sym, Plain impl))
-       [
-         ("+", add);
-         ("-", sub);
-         ("*", mul);
-         ("=?", eq);
-         ("cons", cons);
-         ("car", car);
-         ("cdr", cdr);
-         ("print_int", print_int);
-         ("print_bool", print_bool);
-         ("print_unit", print_unit);
-         ("print_nl", print_nl);
-         ("force", force);
-         ("ref", ref);
-         (":=", ref_set);
-         ("!", ref_get);
-       ]
+  List.fold_left
+    (fun env (name, binding) -> Env.extend env name binding)
+    Env.empty
+    [
+      ("+", add);
+      ("-", sub);
+      ("*", mul);
+      ("=?", eq);
+      ("cons", cons);
+      ("car", car);
+      ("cdr", cdr);
+      ("print_int", print_int);
+      ("print_bool", print_bool);
+      ("print_unit", print_unit);
+      ("print_nl", print_nl);
+      ("force", force);
+      ("ref", ref);
+      (":=", ref_set);
+      ("!", ref_get);
+    ]
 
 let eval = eval stdlib
 let parse program = Parser.prog Lexer.read (Lexing.from_string program)
